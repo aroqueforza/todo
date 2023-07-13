b6ac39c249b4ddba95003e6f68777ecacb776abd
Adalberto Roque <aroque@forzatrans.com>
2023-07-13 09:32:51 -0500

    agregar servicios

--- src/app/tasks-manager/tasks-manager.component.ts
+++ src/app/tasks-manager/tasks-manager.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Task } from '../shared/models/task';
+import { TaskService } from '../shared/services/task.service';
 
 @Component({
   selector: 'app-tasks-manager',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./tasks-manager.component.scss']
 })
 export class TasksManagerComponent {
+  tasks: Task[] = [];
 
+  constructor(private taskService:TaskService ){
+
+  }
 }
